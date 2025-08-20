cb045ac49f4921a633f5e8c855fe9696d0415533
girimugundankumar <girimugundan@vt.edu>
2025-08-20 10:09:48 -0400

    latest commit

--- src/sim-uav/sim-platforms/sim-qrbp/sim-qrbp.cpp
+++ src/sim-uav/sim-platforms/sim-qrbp/sim-qrbp.cpp
@@ -51,31 +51,24 @@ void simqrbp::ConfigureQRBPChassis()
 
     // Configure the initial position of the chassis (NED FRAME) -----------------------------------------------------
     chassis_properties.init_pos = chrono::ChVector3d(0.0984471609373536,0.187071795527612,0.17256855449029);
-    ConfigureUAVChassisInitPos(chassis_properties.init_pos);
-
+    
     // Configure the initial rotation of the chasssis (0 ROTATION IN NED FRAME) ---------------------------------------
     chassis_properties.init_rot = chrono::ChQuaternion<>(1,0,0,0);
-    ConfigureUAVChassisInitRot(chassis_properties.init_rot);
-
+    
     // Configure the mass of the chassis ------------------------------------------------------------------------------
     chassis_properties.mass = 1.67236138725861;
-    ConfigureUAVChassisMass(chassis_properties.mass);
 
     // Configure the Chassis Inertia vectors --------------------------------------------------------------------------
     chassis_properties.InertiaXX = chrono::ChVector3d(0.0233070324786724,0.0160580843956144,0.0271717864469372);
     chassis_properties.InertiaXY = chrono::ChVector3d(1.10006555036583e-07,-0.000482088295393743,1.23260346821255e-06);
-    ConfigureUAVChassisInertiaXX(chassis_properties.InertiaXX);
-    ConfigureUAVChassisInertiaXY(chassis_properties.InertiaXY);
 
     // Configure the chassis COM along with it's auxilliary frame at the COM ------------------------------------------
     // This is the most important as we will consider this to be the pixhawk in our subsequent calculations
     chassis_properties.COM  = chrono::ChFramed(chrono::ChVector3d(-0.0163151142227842,0.0102080920244478,0.017016814810521),
                                                chrono::ChQuaternion<>(1,0,0,0));                            
-    ConfigureUAVChassisCOM(chassis_properties.COM);
 
     // Configure the name of the visualization obj file ---------------------------------------------------------------
     chassis_properties.vis_obj_name = "body_3_1.obj";
-    ConfigureUAVChassisOBJName(chassis_properties.vis_obj_name);
 
     // Configure the chassis collision shapes -------------------------------------------------------------------------
     // Cache some variables for this 
@@ -94,6 +87,7 @@ void simqrbp::ConfigureQRBPChassis()
     dim = chrono::ChVector3d(0.0164047740043827,0.122065278616947,0.5);
     col = chrono_types::make_shared<chrono::ChCollisionShapeBox>(mat,dim);
     frame = chrono::ChFramed(pos, mr);
+
     chassis_properties.collision.emplace_back(col, frame);
 
     // 2
@@ -104,6 +98,7 @@ void simqrbp::ConfigureQRBPChassis()
     dim = chrono::ChVector3d(0.224249837982941,0.265117422382603,0.114399999999996);
     col = chrono_types::make_shared<chrono::ChCollisionShapeBox>(mat,dim);
     frame = chrono::ChFramed(pos,mr);
+
     chassis_properties.collision.emplace_back(col, frame);
 
     // 3
@@ -114,8 +109,17 @@ void simqrbp::ConfigureQRBPChassis()
     dim = chrono::ChVector3d(0.0164047740043827,0.122065278616947,0.5);
     col = chrono_types::make_shared<chrono::ChCollisionShapeBox>(mat,dim);
     frame = chrono::ChFramed(pos,mr);
+    
     chassis_properties.collision.emplace_back(col, frame);
 
+    // Call all the configuration helper functions --------------------------------------------------------------------
+    ConfigureUAVChassisInitPos(chassis_properties.init_pos);
+    ConfigureUAVChassisInitRot(chassis_properties.init_rot);
+    ConfigureUAVChassisMass(chassis_properties.mass);
+    ConfigureUAVChassisInertiaXX(chassis_properties.InertiaXX);
+    ConfigureUAVChassisInertiaXY(chassis_properties.InertiaXY);                                               
+    ConfigureUAVChassisCOM(chassis_properties.COM);
+    ConfigureUAVChassisOBJName(chassis_properties.vis_obj_name);
     ConfigureUAVChassisCollisionShapes(chassis_properties.collision);
 
     // Initiate the UAV chassis ---------------------------------------------------------------------------------------
